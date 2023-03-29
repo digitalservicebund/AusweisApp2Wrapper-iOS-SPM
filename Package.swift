82e560d1b328d2a2b44815689717af26e4dc9615
Andreas Ganske <a.ganske@chaosspace.de>
2023-03-29 10:12:44 +0200

    Update Package.swift to be able to use it in Xcode 14.2+

--- Package.swift
+++ Package.swift
@@ -1,17 +1,16 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-	name: "AusweisApp2SDKWrapper",
+	name: "AA2SDKWrapper",
 	products: [
 		.library(
-			name: "AusweisApp2SDKWrapper",
+			name: "AA2SDKWrapper",
 			targets: ["AusweisApp2Dependency", "AusweisApp2SDKWrapper"]
 		)
 	],
 	dependencies: [
 		.package(
-			name: "AusweisApp2",
 			url: "https://github.com/Governikus/AusweisApp2-SDK-iOS",
 			from: "1.26.3"
 		)
@@ -19,7 +18,10 @@ let package = Package(
 	targets: [
 		.target(
 			name: "AusweisApp2Dependency", // Dummy target, so we can depend on the AusweisApp2, as binaryTarget can't.
-			dependencies: ["AusweisApp2"]
+			dependencies: [
+                .product(name: "AusweisApp2",
+                         package: "AusweisApp2-SDK-iOS")
+            ]
 		),
 		.binaryTarget(
 			name: "AusweisApp2SDKWrapper",
